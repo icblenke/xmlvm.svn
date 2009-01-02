83f44e8b9c715668b75475a30c57066053f14c62
apuder <apuder@0874c2aa-48f6-4a0f-8a6b-e32f4465a257>
2009-01-02 15:04:03 +0000

    Merge latest changes from changes/joshm/android
    git-svn-id: https://xmlvm.svn.sourceforge.net/svnroot/xmlvm@86 0874c2aa-48f6-4a0f-8a6b-e32f4465a257

--- changes/apuder/build/doc/homepage/android/index.php
+++ changes/apuder/build/doc/homepage/android/index.php
@@ -1,3 +1,11 @@
+<style>
+  pre {
+    width:480px;
+    font-family:courier new;
+    font-size:8pt;
+    display:inline;
+  }
+</style>
 <?php
 	include("../header.html");
 ?>
@@ -9,18 +17,24 @@
   <td id="main_content_td">
     <div id="main_content">
 	
-<h1>How do you run an Android application on the iPhone?</h1>
-	<h2>We started with an Android application...</h2>
+<h1>Details: Android to iPhone</h1>
+
+
+<p>How do you run an Android application on the iPhone? The following steps show what we have done.</p>
+<h2>Step 1: Built the Android application</h2>
 	<table>
-	<tr><td>Running in Android emulator</td><td>Java source for Android</td></tr>
 	<tr>
-	<td  valign =top >
-	<img width = 300 src ="androidEmulator.png" /></td>
-	<td>
-	<textarea readonly = "true" rows = "30" cols = "60">
+	  <td><b>Running in Android emulator</b></td>
+	  <td><b>Java source for Android</b></td>
+	</tr>
+	<tr>
+	  <td  valign=top >
+	    <img width=300 src="androidEmulator.jpg" />
+	  </td>
+	  <td>
+	  <pre>
 public class AndroidFireworks extends Activity {
 	/** Called when the activity is first created. */
-
 	public static final int UPDATE = 1337;
 
 	Handler updater = new Handler();
@@ -30,7 +44,6 @@ public class AndroidFireworks extends Activity {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-
 		super.onCreate(savedInstanceState);
 		layout = new AbsoluteLayout(this);
 		setContentView(layout);
@@ -46,27 +59,23 @@ public class AndroidFireworks extends Activity {
 
 		}
 	};
-
 }
-</textarea>
+</pre>
 	</td>
 	</tr>
 	</table>
-	
-	
-	
-	
-	<h2>
-	Implemented the Android API in terms of the Java API for iPhone
-	</h2>
+	<h2>Step 2:	Compatibility library</h2>
+	<p>Next we implemented the Android API in terms of the Java API for iPhone</p>
 	<table>
-	<tr><td>Running in the XMLVM iPhone emulator</td><td>Example of Android compatibility library implemented in terms of the XMLVM iPhone library</td></tr>
-	
 	<tr>
-	<td  valign =top >
-	<img width = 300 src ="iphoneJavaEmulator.png" /></td>
+	  <td><b>Running in the XMLVM iPhone emulator</b></td>
+	  <td><b>Example of Android compatibility library</b></td>
+	</tr>
+	<tr>
+	<td valign=top >
+	<img width=300 src="iphoneJavaEmulator.jpg" /></td>
 	<td>
-	<textarea readonly = "true" rows = "27" cols = "60">
+	<pre>
 package android.app;
 
 public class Activity extends ContextThemeWrapper {
@@ -90,28 +99,24 @@ public class Activity extends ContextThemeWrapper {
 		mainView = view;
 		window.setContentView(view.mainView);
 	}
-
 }
-
-    </textarea>
+  </pre>
 	</td>
 	</tr>
 	</table>
 	
-	
-	
-	
-	<h2>
-	Then cross compiled the application to objective C and compiled it with xCode and the Apple iPhone SDK
-	</h2>
+	<h2>Step3: Cross-compilation</h2>
+	<p>We then cross compiled the application to Objective-C and compiled it with Xcode and the Apple iPhone SDK</p>
 	<table>
-	<tr><td>Running in the iPhone simulator</td><td>Example of cross compiled run method in objective C</td></tr>
-	
 	<tr>
-	<td valign =top >
-	<img width = 300 src ="iphoneEmulator.png" /></td>
+	  <td><b>Running in the iPhone simulator</b></td>
+	  <td><b>Example of cross compiled run method in Objective-C</b></td>
+	</tr>
+	<tr>
+	<td valign=top >
+	<img width=250 src="iphoneEmulator.jpg" /></td>
 	<td>
-	<textarea readonly = "true" rows = "47" cols = "60">
+	<pre>
 - (void) run
 {
     XMLVMElem _stack[4];
@@ -119,7 +124,7 @@ public class Activity extends ContextThemeWrapper {
     int _sp = 0;
     XMLVMElem _op1, _op2, _op3;
     int _i;
-    for (_i = 0; _i <1; _i++) _locals[_i].o = nil;
+    for (_i = 0; _i &lt;1; _i++) _locals[_i].o = nil;
     NSAutoreleasePool* _pool = [[NSAutoreleasePool alloc] init];
     _locals[0].o = self;
     label0:;
@@ -142,15 +147,16 @@ public class Activity extends ContextThemeWrapper {
     _stack[_sp++].o = _locals[0].o;
     _stack[_sp++].l = 50;
     _sp -= 3;
-    _op1.i = [((android_os_Handler*) _stack[_sp].o) postDelayed___java_lang_Runnable_long:_stack[_sp + 1].o:_stack[_sp + 2].l];
+    _op1.i = [((android_os_Handler*) _stack[_sp].o)</pre><img src="wrap.gif" style="display:inline"/><br/>
+    <pre>
+        postDelayed___java_lang_Runnable_long:_stack[_sp + 1].o:_stack[_sp + 2].l];
     _stack[_sp++].i = _op1.i;
-
-_sp--;
+    _sp--;
     label1:;
     [_pool release];
     return;
 }
-    </textarea>
+    </pre>
 	</td>
 	</tr>
 	</table>
@@ -170,15 +176,13 @@ _sp--;
     <li>execute Ant by entering the <b>ant</b> -- the project will build</li>
     <li>A directory called dist will be created, containing the following sub-directories:</li>
         <ul>
-        <li>android-compat: Objective C android compatibility library </li>
+        <li>android-compat: Objective-C android compatibility library </li>
         <li>androidfireworks: </li>
             <ul>
             <li> androidfireworks.jar:  Android application running in the XMLVM iPhone simulator</li>
-            <li> objc: Adding everything in this directory to an xCode iPhone project will let you compile the Android fireworks project for the iPhone or iPhone simulator in xCode</li>
+            <li> objc: Adding everything in this directory to an Xcode iPhone project will let you compile the Android fireworks project for the iPhone or iPhone simulator in Xcode</li>
             </ul>
         </ul>
-        
-    
     </ul>
 	
 <?php
